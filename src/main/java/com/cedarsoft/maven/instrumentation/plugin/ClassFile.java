01153176fe9de194bcd16eb3988d941179da3e26
Johannes Schneider <js@cedarsoft.com>
2012-08-25 00:15:24 +0200

    added base

---
+++ src/main/java/com/cedarsoft/maven/instrumentation/plugin/ClassFile.java
@@ -0,0 +1,118 @@
+package com.cedarsoft.maven.instrumentation.plugin;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.LoaderClassPath;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.ProtectionDomain;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class ClassFile {
+  @Nonnull
+  private final File classFile;
+  @Nonnull
+  private final CtClass compiledClass;
+
+  public ClassFile( @Nonnull final File classFile, @Nonnull ClassLoader classLoader ) throws IOException {
+    this.classFile = classFile;
+    final ClassPool cp = ClassPool.getDefault();
+    cp.appendClassPath( new LoaderClassPath( classLoader ) );
+
+    final InputStream ins = new BufferedInputStream( new FileInputStream( classFile ) );
+    try {
+      compiledClass = cp.makeClass( ins );
+    } finally {
+      ins.close();
+    }
+  }
+
+  @Nonnull
+  public String getClassName() {
+    return compiledClass.getName();
+  }
+
+  public void transform( @Nonnull final ClassFileTransformer agent ) throws ClassTransformationException {
+    try {
+      doTransform( agent );
+    } catch ( final MalformedURLException e ) {
+      throw new RuntimeException( "Should not happen.  Could not create URL from file.", e );
+    } catch ( final IOException e ) {
+      throw new ClassTransformationException( "Failed to write transformed class file contents.", e );
+    } catch ( final CannotCompileException e ) {
+      throw new RuntimeException( "Should not happen. Failed to convert a valid compiled class into bytecode", e );
+    } catch ( final IllegalClassFormatException e ) {
+      throw new RuntimeException( "Should not happen.  The class file does not contain valid content", e );
+    }
+  }
+
+  private void doTransform( @Nonnull final ClassFileTransformer agent ) throws MalformedURLException, CannotCompileException, IOException,
+    IllegalClassFormatException {
+    final ClassLoader loader = getClassLoader();
+    final String className = getInternalClassName();
+    final Class<?> classBeingRedefined = compiledClass.toClass( loader, loader.getClass().getProtectionDomain() );
+    final byte[] classBytes = compiledClass.toBytecode();
+    final ProtectionDomain protectionDomain = classBeingRedefined.getProtectionDomain();
+
+    final byte[] transformedBytes = agent.transform( loader, className, classBeingRedefined, protectionDomain, classBytes );
+
+    replaceClassContents( transformedBytes );
+  }
+
+  @Nonnull
+  private ClassLoader getClassLoader() throws MalformedURLException {
+    final File parentDir = getClassParentDir();
+    return new URLClassLoader( new URL[]{parentDir.toURI().toURL()} );
+  }
+
+  @Nonnull
+  public File getClassParentDir() {
+    final String className = compiledClass.getName();
+    final int packageCount = className.split( "\\." ).length;
+    File parentDir = classFile;
+    for ( int i = 0; i < packageCount; i++ ) {
+      parentDir = parentDir.getParentFile();
+    }
+    return parentDir;
+  }
+
+  /**
+   * @return The internal class name as specified in the JVM Specification
+   *
+   * @see http://java.sun.com/docs/books/jvms/second_edition/html/ClassFile.doc.html
+   */
+  @Nonnull
+  private String getInternalClassName() {
+    return compiledClass.getName().replace( '.', '/' );
+  }
+
+  private void replaceClassContents( @Nonnull final byte[] replacementBytes ) throws IOException {
+    final BufferedOutputStream bufferedStream = new BufferedOutputStream( new FileOutputStream( classFile ) );
+    try {
+      bufferedStream.write( replacementBytes );
+      bufferedStream.flush();
+    } finally {
+      bufferedStream.close();
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "ClassFile [classFile=" + classFile + "]";
+  }
+}
---
+++ src/main/java/com/cedarsoft/maven/instrumentation/plugin/InstrumentationMojo.java
@@ -0,0 +1,112 @@
+package com.cedarsoft.maven.instrumentation.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ * @goal instrument
+ * @phase process-classes
+ */
+public class InstrumentationMojo extends AbstractMojo {
+  /**
+   * The fully qualified class names of the transformers to apply.
+   *
+   * @parameter
+   * @required
+   */
+  private List<String> classTransformers;
+
+  /**
+   * @parameter expression="${project.build.outputDirectory}"
+   * @read-only
+   * @required
+   */
+  private String outputDirectory;
+
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    getLog().info( "Starting InstrumentationMojo" );
+
+    final Collection<ClassFileTransformer> agents = getAgents();
+    final File outputDirectoryDir = new File( outputDirectory );
+    final Collection<? extends ClassFile> classFiles = createLocator().findClasses( outputDirectoryDir );
+
+    performClassTransformation( classFiles, agents );
+  }
+
+  private static void performClassTransformation( @Nonnull final Iterable<? extends ClassFile> classFiles, @Nonnull final Iterable<? extends ClassFileTransformer> agents ) throws MojoExecutionException {
+    for ( final ClassFile classFile : classFiles ) {
+      for ( final ClassFileTransformer agent : agents ) {
+        transformClass( classFile, agent );
+      }
+    }
+  }
+
+  private static void transformClass( @Nonnull final ClassFile classFile, @Nonnull final ClassFileTransformer agent ) throws MojoExecutionException {
+    try {
+      classFile.transform( agent );
+    } catch ( final ClassTransformationException e ) {
+      final String message = MessageFormat.format( "Failed to transform class: {0}, using ClassFileTransformer, {1}", classFile, agent.getClass() );
+      throw new MojoExecutionException( message, e );
+    }
+  }
+
+  private Collection<ClassFileTransformer> getAgents() throws MojoExecutionException {
+    final Collection<ClassFileTransformer> agents = new ArrayList<ClassFileTransformer>();
+    for ( final String className : classTransformers ) {
+      final ClassFileTransformer instance = createAgentInstance( className );
+      agents.add( instance );
+    }
+    return agents;
+  }
+
+  private static ClassFileTransformer createAgentInstance( final String className ) throws MojoExecutionException {
+    final Class<?> agentClass = resolveClass( className );
+    if ( !ClassFileTransformer.class.isAssignableFrom( agentClass ) ) {
+      final String message = className + "is not an instance of " + ClassFileTransformer.class;
+      throw new MojoExecutionException( message );
+    }
+    return toClassFileTransformerInstance( agentClass );
+  }
+
+  private static ClassFileTransformer toClassFileTransformerInstance(
+    final Class<?> agentClass ) throws MojoExecutionException {
+    try {
+      return ( ClassFileTransformer ) agentClass.getConstructor().newInstance();
+    } catch ( final InstantiationException e ) {
+      throw new MojoExecutionException( "Failed to instantiate class: " + agentClass + ". Does it have a no-arg constructor?", e );
+    } catch ( final IllegalAccessException e ) {
+      throw new MojoExecutionException( agentClass + ". Does not have a public no-arg constructor?", e );
+    } catch ( NoSuchMethodException e ) {
+      throw new MojoExecutionException( "Failed to instantiate class: " + agentClass + ". Does it have a no-arg constructor", e );
+    } catch ( InvocationTargetException e ) {
+      throw new MojoExecutionException( "Failed to instantiate class: " + agentClass + ". Could not invoke constructor", e );
+    }
+  }
+
+  private static Class<?> resolveClass( @Nonnull final String className ) throws MojoExecutionException {
+    try {
+      return Class.forName( className );
+    } catch ( final ClassNotFoundException e ) {
+      final String message = MessageFormat.format( "Could not find class: {0}. Is it a registered dependency of the project or the plugin?", className );
+      throw new MojoExecutionException( message, e );
+    }
+  }
+
+  @Nonnull
+  private ClassFileLocator createLocator() {
+    return new ClassFileLocator( getLog(), getClass().getClassLoader() );
+  }
+}
